80a912b62c2070cf375a8ef4b67d25cee10b3d70
Muratcan Agiç <muratcanagic@hotmail.com>
2023-08-06 18:45:24 +0300

    Model and views added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,6 +7,7 @@ namespace MvcEticaret.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        List<ProductItem> _products = new List<ProductItem>();
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -15,7 +16,17 @@ namespace MvcEticaret.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            _products.AddRange(new List<ProductItem> {
+                new ProductItem { Id = 1, imageUrl = "../images/mouse.png", isSale= true, OldPrice=20.50m, Price=15.00m, StartCount=3, Title="Mouse" },
+                new ProductItem { Id = 2, imageUrl = "../images/keyboard.png", isSale= false, OldPrice=30.00m, Price=30.00m, StartCount=5, Title="Keyboard" },
+                new ProductItem { Id = 3, imageUrl = "../images/ram.jpg", isSale= true, OldPrice=14.99m, Price=12.00m, StartCount=0, Title="Ram" },
+                new ProductItem { Id = 4, imageUrl = "../images/ssd.png", isSale= false, OldPrice=25.00m, Price=25.00m, StartCount=4, Title="SSD" },
+                new ProductItem { Id = 5, imageUrl = "../images/processor.png", isSale= true, OldPrice=155.99m, Price=140.50m, StartCount=2, Title="Processor" },
+                new ProductItem { Id = 6, imageUrl = "../images/headphone.png", isSale= false, OldPrice=28.00m, Price=28.00m, StartCount=5, Title="Headphone" },
+                new ProductItem { Id = 7, imageUrl = "../images/monitor.png", isSale= true, OldPrice=132.00m, Price=110.00m, StartCount=0, Title="Monitor" },
+                new ProductItem { Id = 8, imageUrl = "../images/graphicCard.png", isSale= false, OldPrice=430.00m, Price=430.00m, StartCount=5, Title="Graph Card" }
+            });
+            return View(_products);
         }
 
         public IActionResult Privacy()
---
+++ Models/ProductItem.cs
@@ -0,0 +1,14 @@
+﻿namespace MvcEticaret.Models
+{
+    public class ProductItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int StartCount { get; set; }
+        public decimal OldPrice { get;set; }
+        public decimal Price { get;set; }
+        public string imageUrl { get; set; }
+        public bool isSale { get; set; 
+        }
+    }
+}
